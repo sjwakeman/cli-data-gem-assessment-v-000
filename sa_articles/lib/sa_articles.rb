1dbac940f239e3a7f898b753330029f8a078d22b
Scott Wakeman <swakeman@ca.rr.com>
2017-04-11 23:21:57 +0000

    nokogiri article and cli

--- sa_articles/lib/sa_articles.rb
+++ sa_articles/lib/sa_articles.rb
@@ -1,5 +1,8 @@
-require "sa_articles/version"
+require 'nokogiri'
 
+require "sa_articles/version"
+require_relative './sa_articles/article'
+require_relative './sa_articles/cli'
 module SaArticles
   # Your code goes here...
 end
