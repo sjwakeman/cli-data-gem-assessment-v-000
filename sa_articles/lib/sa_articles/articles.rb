255ace47a692186696f45c143853ec88b97b0068
Scott Wakeman <swakeman@ca.rr.com>
2017-04-26 04:14:22 +0000

    articles.rb

--- sa_articles/lib/sa_articles/articles.rb
+++ sa_articles/lib/sa_articles/articles.rb
@@ -1,13 +1,27 @@
-class SeekingAlphaArticles::Article
+class SeekingAlphaArticles::Articles
 
   attr_accessor :title, :url, :author
   @@all = []
 
   def self.new_from_index_page(r)
     self.new(
-      r.css("a.a-title").text.strip,
-      r.css('div.a-info a').text.strip,
-      r.css('a.a-title').attr("href")
-      )
+       r.css("a.a-title").text.strip,
+       r.css('a.a-title').attr("href"),
+       r.css('div.a-info a').text.strip
+       )
+  end
+
+  def initialize(title=nil, url=nil, url=nil, author=nil)
+    @title = title
+    @url = url
+    @author = author
+  end
+
+  def self.all
+    @@all
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
   end
 end
