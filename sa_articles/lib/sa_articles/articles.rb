42c06830d9ad7a7358a6bba40c5bb74d4f43053b
Scott Wakeman <swakeman@ca.rr.com>
2017-04-26 00:36:24 +0000

    comma deletion

--- sa_articles/lib/sa_articles/articles.rb
+++ sa_articles/lib/sa_articles/articles.rb
@@ -6,8 +6,8 @@ class SeekingAlphaArticles::Article
   def self.new_from_index_page(r)
     self.new(
       r.css("a.a-title").text.strip,
-      r.css('div.a-info a').text.strip
-      r.css('a.a-title').attr("href"),
+      r.css('div.a-info a').text.strip,
+      r.css('a.a-title').attr("href")
       )
   end
 end
